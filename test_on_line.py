a9bb837083e4072b3bc98b5de23d94f0ea0ad489
Alissa Kong <akong1399@gmail.com>
2021-09-14 18:44:37 -0400

    Modified formatting of unit test to fix any reported pycodestyle errors. Also missed changing the (x1,y1),(x2,y2) tuple to x1,y1,x2,y2 in one instance during the previous edit, so changed that while running the unit test as well.

--- test_on_line.py
+++ test_on_line.py
@@ -1,10 +1,11 @@
 import pytest
 
+
 @pytest.mark.parametrize("x1, y1, x2, y2, x, expected", [
-    (0, 0, 5, 0, -3, 0), # Testing horizontal line
-    (1, 1, 2, 3, 3, 5), # Testing line with postive slope
-    (-5, 5, 0, 0, 8, -8)]) # Testnig line with negative slope
+    (0, 0, 5, 0, -3, 0),  # Testing horizontal line
+    (1, 1, 2, 3, 3, 5),  # Testing line with postive slope
+    (-5, 5, 0, 0, 8, -8)])  # Testnig line with negative slope
 def test_y_on_line(x1, y1, x2, y2, x, expected):
     from on_line import y_on_line
-    answer = y_on_line((x1,y1), (x2,y2), x)
+    answer = y_on_line(x1, y1, x2, y2, x)
     assert answer == expected
